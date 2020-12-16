c5fbc17118838665310d55c39b3424b01e16e213
Christine Dodrill <me@christine.website>
2020-12-16 15:36:04 -0500

    gemini: document package
    Signed-off-by: Christine Dodrill <me@christine.website>

---
+++ gemini/doc.go
@@ -0,0 +1,7 @@
+// Package gemini is a simple gemini server framework in the style of net/http.
+//
+// This server conforms to the gemini protocol version v0.14.3 as described
+// here: gemini://gemini.circumlunar.space/docs/specification.gmi
+//
+// TODO(Xe): add examples here
+package gemini
--- gemini/server.go
+++ gemini/server.go
@@ -26,12 +26,16 @@ type Server struct {
 	hdl Handler
 }
 
+// NewServer creates a new Gemini server based on a particular handler.
 func NewServer(hdl Handler) *Server {
 	return &Server{
 		hdl: hdl,
 	}
 }
 
+// ListenAndServe creates a new TLS listener on a given port with a given TLS certificate
+// and key. This is most useful for serving a single site. If you need more control or
+// want to host multiple sites, create your own tls.Listener and use the Serve method.
 func (s *Server) ListenAndServe(addr string, certPath string, keyPath string) error {
 	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
 	if err != nil {
@@ -47,6 +51,7 @@ func (s *Server) ListenAndServe(addr string, certPath string, keyPath string) er
 	return s.Serve(lis)
 }
 
+// Serve serves gemini responses to clients connecting to this Listener.
 func (s *Server) Serve(lis net.Listener) error {
 	if s.lis != nil {
 		return fmt.Errorf("listener already set")
@@ -97,12 +102,24 @@ func (s *Server) handle(conn net.Conn) {
 	s.hdl.HandleGemini(cw, req)
 }
 
+// Request contains all relevant metadata for a gemini request.
 type Request struct {
 	URL  *url.URL
 	Cert *x509.Certificate
 }
 
+// ResponseWriter is used by a gemini handler to construct a gemini response.
+//
+// This may not be used after the HandleGemini method has returned.
 type ResponseWriter interface {
+	// Status sends the status line to the client with the provided status
+	// code and metadata.
+	//
+	// Only one status line may be sent to the client. Making more than one
+	// call to this method should result in a panic.
+	//
+	// The provided code SHOULD be one of the status codes defined in this
+	// package.
 	Status(status int, meta string)
 	io.Writer
 }
@@ -129,10 +146,19 @@ func (cw *connWrapper) Status(status int, meta string) {
 	cw.status = status
 }
 
+// A Handler responds to a gemini request.
+//
+// HandleGemini should write the status line and any relevant body
+// data then return.
+//
+// The provided request is safe to modify provided you really
+// understand what you are doing.
 type Handler interface {
 	HandleGemini(rw ResponseWriter, req *Request)
 }
 
+// HandlerFunc is a convenience wrapper that lets you easily construct
+// Handlers from ad-hoc functions.
 type HandlerFunc func(ResponseWriter, *Request)
 
 func (hf HandlerFunc) HandleGemini(w ResponseWriter, r *Request) { hf(w, r) }
